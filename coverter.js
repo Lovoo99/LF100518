535af9e39b5fb3c3c2d1347a52fcf83d50cb25c8
Lovoo99 <luislovof@gmail.com>
2024-09-07 20:44:50 -0600

    Delete coverter.js

--- coverter.js
+++
@@ -1,8 +0,0 @@
-const inputField = document.getElementById("input-temp") //la constante devuelve las propiedades del elemento con id input-temp, con class se repite el elemento
-console.log(inputField); //se puede colocar cualquier mensaje entre ()
-const fromUnitField = document.getElementById("input-unit");
-const toUnitField = document.getElementById("output-unit");
-const outputField = document.getElementById("output-temp");
-const form = document.getElementById("converter");
-
-console.log("Luis Eduardo Lovo Figueroa);
